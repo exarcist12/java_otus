db409d4ecafbd4062856136ef65fa7375f91d7b6
exarcist12 <exarcist12@gmail.com>
2023-01-06 15:25:08 +0400

    первый тест почти отработал

--- otuspracticeproject/src/main/java/components/MenuComponent.java
+++ otuspracticeproject/src/main/java/components/MenuComponent.java
@@ -4,10 +4,14 @@ import data.CategoryData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.CategoryPage;
 import waiters.StandartWaiter;
 
 import java.util.List;
+
+import static waiters.WaiterInt.IMPLICITLY_WAIT_SECOND;
 
 
 public class MenuComponent extends AbsBaseComponent<MenuComponent> {
@@ -16,7 +20,7 @@ public class MenuComponent extends AbsBaseComponent<MenuComponent> {
         super(driver);
     }
 
-    StandartWaiter standartWaiter = new StandartWaiter(driver);
+
     private final String menuItemByTitleSelectorTemplate = "#categories_id a[title=\"%s\"]";
     private final String catalogCategoriesCheckboxesSelectorTemplate = "//label[text()=\"%s\"]/..//div/input[@checked]";
 
@@ -32,8 +36,16 @@ public class MenuComponent extends AbsBaseComponent<MenuComponent> {
         String selector = String.format(catalogCategoriesCheckboxesSelectorTemplate, categoryData.getName());
         WebElement element = driver.findElement(By.xpath(selector));
         List<WebElement> elements = driver.findElements(By.xpath(selector));
-        element.click();
-        assert standartWaiter.waitForElementVisible(element): "Error";
+//        element.click();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, IMPLICITLY_WAIT_SECOND);
+        boolean condPerf;
+        try {
+            webDriverWait.until(ExpectedConditions.visibilityOf(element));
+            condPerf = true;
+        } catch (Exception ex) {
+            condPerf =  false;
+        }
+//       assert standartWaiter.waitForElementVisible(element): "Error";
         return this;
     }
 
