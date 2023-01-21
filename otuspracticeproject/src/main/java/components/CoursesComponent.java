0636dfc0f5783b52ae4988827c66a56cd7856072
exarcist12 <exarcist12@gmail.com>
2023-01-21 14:35:26 +0400

    пофишкены компоненты

--- otuspracticeproject/src/main/java/components/CoursesComponent.java
+++ otuspracticeproject/src/main/java/components/CoursesComponent.java
@@ -8,66 +8,65 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class CoursesComponent extends AbsBaseComponent<CoursesComponent> {
+  public CoursesComponent(WebDriver driver) {
+    super(driver);
+  }
 
-   public CoursesComponent(WebDriver driver) {
-      super(driver);
-   }
 
+  public String getDateString(String dateString){
+    List<String> listString= new ArrayList<>(Arrays.asList(dateString.split(" ")));
+    if (listString.get(0).equals("")){
+      listString.remove(0);
+    }
 
-   public String getDateString(String dateString){
-      List<String> listString= new ArrayList<>(Arrays.asList(dateString.split(" ")));
-      if (listString.get(0).equals("")){
-         listString.remove(0);
+    if (listString.get(0).equals("С")){
+      listString.remove(0);
+    }
+
+    if (listString.size()>2){
+      int i = 0;
+      while ((i<5) && (listString.size()>3)) {
+        i++;
+        listString.remove(3);
       }
 
-      if (listString.get(0).equals("С")){
-         listString.remove(0);
+      if (listString.get(0).equals("В")){
+        listString.set(0, "1");
       }
 
-      if (listString.size()>2){
-         int i = 0;
-         while ((i<5) && (listString.size()>3)) {
-            i++;
-            listString.remove(3);
-         }
-
-         if (listString.get(0).equals("В")){
-            listString.set(0, "1");
-         }
-
-         for(int j = 3; j<listString.size(); j++){
-            listString.remove(j);
-         }
-
-         if (Integer.valueOf(listString.get(2)) < 2022) {
-            listString.remove(2);
-            listString.add("2023");
-         }
-
-         if (listString.get(1).equals("октябре")){
-            listString.set(1, "октября");
-         }
+      for(int j = 3; j<listString.size(); j++){
+        listString.remove(j);
       }
 
-      if ((listString.size()<3)) {
-         listString.add("2023");
+      if (Integer.valueOf(listString.get(2)) < 2022) {
+        listString.remove(2);
+        listString.add("2023");
       }
 
-      String date = String.join(" ", listString);
-      return date;
-   }
-
-   public Date getDate(String dateString) throws ParseException {
-      Date date;
-      try {
-         DateFormat format = new SimpleDateFormat("dd MMMM yyyy", new Locale("ru"));
-         date = format.parse(dateString);
+      if (listString.get(1).equals("октябре")){
+        listString.set(1, "октября");
       }
-      catch(ParseException e) {
-         DateFormat format = new SimpleDateFormat("dd MMMM yyyy", new Locale("en"));
-         date = format.parse(dateString);
-      }
+    }
 
-      return date;
-   }
- }
+    if ((listString.size()<3)) {
+      listString.add("2023");
+    }
+
+    String date = String.join(" ", listString);
+    return date;
+  }
+
+  public Date getDate(String dateString) throws ParseException {
+    Date date;
+    try {
+      DateFormat format = new SimpleDateFormat("dd MMMM yyyy", new Locale("ru"));
+      date = format.parse(dateString);
+    }
+    catch(ParseException e) {
+      DateFormat format = new SimpleDateFormat("dd MMMM yyyy", new Locale("en"));
+      date = format.parse(dateString);
+    }
+
+    return date;
+  }
+}
--- otuspracticeproject/src/main/java/components/MenuComponent.java
+++ otuspracticeproject/src/main/java/components/MenuComponent.java
@@ -1,5 +1,8 @@
 package components;
 
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import data.CategoryData;
 import data.Course;
 import data.CoursesData;
@@ -18,187 +21,149 @@ import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
 public class MenuComponent extends AbsBaseComponent<MenuComponent> {
 
-   public MenuComponent(WebDriver driver) {
-      super(driver);
-   }
-   CoursesComponent coursesComponent = new CoursesComponent(driver);
-   StandartWaiter standartWaiter = new StandartWaiter(driver);
-   private final String menuItemByTitleSelectorTemplate = "#categories_id a[title=\"%s\"]";
-   private final String menuCourseSelectorTemplate = "//div[contains(., '%s') and contains(@class, 'lessons__new-item-title')]";
-   private final String catalogCategoriesCheckboxesSelectorTemplate = "//label[text()=\"%s\"]/..//div/input[@checked]";
-   private final String logo = ".header3__logo-img";
-   private final String buttonCoockiesTemplate = "button.cookies__button";
-   private final String titleCourseTemplate = "div.lessons__new-item-title";
-   private final String courseTemplate = "div.lessons__new-item-container";
-   private final String dateStartTemplate = "div.lessons__new-item-start";
-   private final String timeTemplate = "div.lessons__new-item-time";
+  public MenuComponent(WebDriver driver) {
+    super(driver);
+  }
+  CoursesComponent coursesComponent = new CoursesComponent(driver);
+  StandartWaiter standartWaiter = new StandartWaiter(driver);
+  private final String menuItemByTitleSelectorTemplate = "#categories_id a[title=\"%s\"]";
+  private final String menuCourseSelectorTemplate = "//div[contains(., '%s') and contains(@class, 'lessons__new-item-title')]";
+  private final String catalogCategoriesCheckboxesSelectorTemplate = "//label[text()=\"%s\"]/..//div/input[@checked]";
+  private final String logo = ".header3__logo-img";
+  private final String buttonCoockiesTemplate = "button.cookies__button";
+  private final String titleCourseTemplate = "div.lessons__new-item-title";
+  private final String courseTemplate = "div.lessons__new-item-container";
+  private final String dateStartTemplate = "div.lessons__new-item-start";
+  private final String timeTemplate = "div.lessons__new-item-time";
 
-   public CategoryPage clickCategory(CategoryData categoryData) {
+  public CategoryPage clickCategory(CategoryData categoryData) {
 
-      String selector = String.format(menuItemByTitleSelectorTemplate, categoryData.getName());
-      driver.findElement(By.cssSelector(selector)).click();
+    String selector = String.format(menuItemByTitleSelectorTemplate, categoryData.getName());
+    driver.findElement(By.cssSelector(selector)).click();
 
-      return new CategoryPage(driver);
-   }
+    return new CategoryPage(driver);
+  }
 
-   public MenuComponent menuItemActive(CategoryData categoryData) {
+  public MenuComponent menuItemActive(CategoryData categoryData) {
 
-      String selector = String.format(catalogCategoriesCheckboxesSelectorTemplate, categoryData.getName());
-      WebElement element = driver.findElement(By.xpath(selector));
-      List<WebElement> elements = driver.findElements(By.xpath(selector));
-      assertThat("error", standartWaiter.waitForElementVisible(element), equalTo(true));
+    String selector = String.format(catalogCategoriesCheckboxesSelectorTemplate, categoryData.getName());
+    WebElement element = driver.findElement(By.xpath(selector));
+    List<WebElement> elements = driver.findElements(By.xpath(selector));
+    assertThat("error", standartWaiter.waitForElementVisible(element), equalTo(true));
 
-      return this;
-   }
+    return this;
+  }
 
-   public List<String> getCoursesStringFromMainPage() {
+  public List<String> getCoursesStringFromMainPage() {
 
-      List<WebElement> listTitleCourses = driver.findElements((By.cssSelector(titleCourseTemplate)));
-      ArrayList<String> titleCourses = new ArrayList<>();
-      String title;
-      for(WebElement element : listTitleCourses) {
-         title = element.getText();
-         titleCourses.add(title);
-      }
+    List<WebElement> listTitleCourses = driver.findElements((By.cssSelector(titleCourseTemplate)));
+    ArrayList<String> titleCourses = new ArrayList<>();
+    String title;
+    for(WebElement element : listTitleCourses) {
+      title = element.getText();
+      titleCourses.add(title);
+    }
 
-      return titleCourses;
-   }
+    return titleCourses;
+  }
 
-   public List<CoursesData> getCoursesDataFromMainPage(List<String> list) {
+  public List<CoursesData> getCoursesDataFromMainPage(List<String> list) {
 
-      List<CoursesData>  listEnums = Arrays.asList(CoursesData.values());
+    List<CoursesData>  listEnums = Arrays.asList(CoursesData.values());
 
-      List<CoursesData> newList = listEnums.stream().filter(s ->
+    List<CoursesData> newList = listEnums.stream().filter(s ->
             list.contains(s.getName())
       ).collect(Collectors.toList());
 
-      return newList;
-   }
+    return newList;
+  }
 
-   public CoursesData filterCourseName(String name) {
+  public CoursesData filterCourseName(String name) {
 
-      List<String> courseString =  getCoursesStringFromMainPage();
-      List<CoursesData> coursesData = getCoursesDataFromMainPage(courseString);
-      List<CoursesData> coursesData2 = coursesData.stream().filter(s -> s.getName() == name).collect(Collectors.toList());
+    List<String> courseString =  getCoursesStringFromMainPage();
+    List<CoursesData> coursesData = getCoursesDataFromMainPage(courseString);
+    List<CoursesData> coursesData2 = coursesData.stream().filter(s -> s.getName() == name).collect(Collectors.toList());
 
-      return coursesData2.get(0);
-   }
-
-
-   public CoursePage clickCourse(CoursesData coursesData) {
-
-      String selector = String.format(menuCourseSelectorTemplate, coursesData.getName());
-      WebElement element = driver.findElement(By.xpath(selector));
-      List<WebElement> buttonCoockies = driver.findElements(By.cssSelector(buttonCoockiesTemplate));
-      if (buttonCoockies.size()>0){
-         buttonCoockies.get(0).click();
-      }
-      new Actions(driver).moveToElement(element).click().perform();
-
-      return new CoursePage(driver);
-   }
-
-   public CoursesComponent checkTitlePage(CoursePage coursePage, CoursesData coursesData) {
-
-      String title = coursePage.getPageTitle();
-      assertThat("Тайтл не совпал", title, equalTo(coursesData.getPageTitle()));
-
-      return new CoursesComponent(driver);
-   }
+    return coursesData2.get(0);
+  }
 
 
-   public MainPage clickMainPage() {
+  public CoursePage clickCourse(CoursesData coursesData) {
 
-      String selector = String.format(logo);
-      driver.findElement(By.cssSelector(selector)).click();
+    String selector = String.format(menuCourseSelectorTemplate, coursesData.getName());
+    WebElement element = driver.findElement(By.xpath(selector));
+    List<WebElement> buttonCoockies = driver.findElements(By.cssSelector(buttonCoockiesTemplate));
+    if (buttonCoockies.size()>0){
+      buttonCoockies.get(0).click();
+    }
+    new Actions(driver).moveToElement(element).click().perform();
 
-      return new MainPage(driver);
-   }
+    return new CoursePage(driver);
+  }
 
-   public ArrayList<Course> coursesWithDate() throws ParseException {
-      List<WebElement> elementsCourse = driver.findElements(By.cssSelector(courseTemplate));
-      ArrayList<Course> coursesWithDate = new ArrayList<>();
-      Course courseWithDate = new Course();
-      CoursesData courseDataData = null;
-      List<CoursesData> coursesDataList = Arrays.asList(CoursesData.values());
+  public CoursesComponent checkTitlePage(CoursePage coursePage, CoursesData coursesData) {
 
 
-      for (WebElement elementCourse: elementsCourse){
-         WebElement elementName = elementCourse.findElement(By.cssSelector(titleCourseTemplate));
-         String nameCourse = elementName.getText();
+    assertThat("Тайтл не совпал", coursePage.getPageTitle(), equalTo(coursesData.getPageTitle()));
 
-         courseDataData = coursesDataList.stream().filter(p -> (p.getName().equals(nameCourse))).findFirst().get();
+    return new CoursesComponent(driver);
+  }
 
-         List<WebElement> elementsDate = elementCourse.findElements(By.cssSelector(dateStartTemplate));
-         if (elementsDate.size()==0){
-            elementsDate = elementCourse.findElements(By.cssSelector(timeTemplate));
-         }
 
-         String dateString = elementsDate.get(0).getAttribute("innerText");
+  public MainPage clickMainPage() {
 
-         Date date = coursesComponent.getDate(coursesComponent.getDateString(dateString));
+    String selector = String.format(logo);
+    driver.findElement(By.cssSelector(selector)).click();
 
-         courseWithDate.setCoursesData(courseDataData);
-         courseWithDate.setDate(date);
-         coursesWithDate.add(courseWithDate);
-         courseWithDate = new Course();
+    return new MainPage(driver);
+  }
+
+  public ArrayList<Course> coursesWithDate() throws ParseException {
+    List<WebElement> elementsCourse = driver.findElements(By.cssSelector(courseTemplate));
+    ArrayList<Course> coursesWithDate = new ArrayList<>();
+    Course courseWithDate = new Course();
+    CoursesData courseDataData = null;
+    List<CoursesData> coursesDataList = Arrays.asList(CoursesData.values());
+
+
+    for (WebElement elementCourse: elementsCourse){
+      WebElement elementName = elementCourse.findElement(By.cssSelector(titleCourseTemplate));
+      String nameCourse = elementName.getText();
+
+      courseDataData = coursesDataList.stream().filter(p -> (p.getName().equals(nameCourse))).findFirst().get();
+
+      List<WebElement> elementsDate = elementCourse.findElements(By.cssSelector(dateStartTemplate));
+      if (elementsDate.size()==0){
+        elementsDate = elementCourse.findElements(By.cssSelector(timeTemplate));
       }
 
-      return coursesWithDate;
-   }
+      String dateString = elementsDate.get(0).getAttribute("innerText");
 
+      Date date = coursesComponent.getDate(coursesComponent.getDateString(dateString));
 
+      courseWithDate.setCoursesData(courseDataData);
+      courseWithDate.setDate(date);
+      coursesWithDate.add(courseWithDate);
+      courseWithDate = new Course();
+    }
 
-//   public String datess(){
-//      List<WebElement> elementsCourse = driver.findElements(By.cssSelector("div.lessons__new-item-container"));
-//
-//      WebElement elementDate = elementsCourse.get(0).findElement(By.cssSelector("div.lessons__new-item-start"));
-//      String date = elementDate.getAttribute("innerText");
-//      return date;
-//   }
+    return coursesWithDate;
+  }
 
-//   public Course maxCourse(List<Course> courses){
-//      Course course = courses.stream().reduce((p1, p2) -> (p2.getDate().getTime() > p1.getDate().getTime() ? p2 : p1)).get();
-//      return course;
-//   }
-//
-//   public Course minCourse(List<Course> courses){
-//      Course course = courses.stream().reduce((p1, p2) -> (p1.getDate().getTime() > p2.getDate().getTime() ? p2 : p1)).get();
-//      return course;
-//   }
-//
-//   public Course minCourse2(List<Course> courses){
-//      BinaryOperator<Course> accumulator = (p1, p2) -> (p1.getDate().getTime() > p2.getDate().getTime() ? p2 : p1);
-//      Course course = courses.stream().reduce(accumulator).get();
-//      return course;
-//   }
+  public Course function(List<Course> courses, BinaryOperator<Long> accumulator){
 
-//   public Long minCourse3(List<Course> courses){
-//      Optional<Long> min = courses.stream().map(p1 -> p1.getDate().getTime()).reduce(Long::min);
-//      Long min = null;
-//      courses.stream().reduce((p1, p2) -> {
-//         Long t1 = p1.getDate().getTime();
-//         Long t2 = p2.getDate().getTime();
-//         Optional<Long> minimum = Optional.of(Long.min(t1, t2));
-//         return minimum;
-//      }).
-//      return min;
-//   }
+    //из списка курсов получаем список дат (в виде миллисекунд в Integer)
+    List<Long> dates = courses.stream().map(p1 ->  p1.getDate().getTime()).collect(Collectors.toList());
 
-   public Course function(List<Course> courses, BinaryOperator<Long> accumulator){
-//из списка курсов получаем список дат (в виде миллисекунд в Integer)
-      List<Long> dates = courses.stream().map(p1 ->  p1.getDate().getTime()).collect(Collectors.toList());
-//из списка дат получаем минимальное максимально
-      Long value = dates.stream().reduce(accumulator).get();
-//по полученной дате получаем курс
-      Course course = courses.stream().filter(p1 -> p1.getDate().getTime() == value).findFirst().get();
+    //из списка дат получаем минимальное максимально
+    Long value = dates.stream().reduce(accumulator).get();
 
-      return course;
-   }
+    //по полученной дате получаем курс
+    Course course = courses.stream().filter(p1 -> p1.getDate().getTime() == value).findFirst().get();
+
+    return course;
+  }
 
 }
